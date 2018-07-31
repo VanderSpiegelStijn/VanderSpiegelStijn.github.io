015c19a8a3c79f1a17a45e206bc6f58958e5b352
unknown <stijn.van.der.spiegel@student.howest.be>
2018-07-31 19:34:10 +0200

    test flexbox divs

---
+++ script/app.js
@@ -0,0 +1,10 @@
+function InitVars(){
+	infoign = document.getElementById("ign-information"),
+	cocking = document.getElementById("cocktails-for-ing"),
+}
+
+
+
+$("sbutton").click(function(){
+    $("ign-information").show();
+});
