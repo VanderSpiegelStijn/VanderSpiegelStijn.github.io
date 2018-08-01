1e2b52cb4335fb07911605a833b4fd9dbbd6e686
unknown <stijn.van.der.spiegel@student.howest.be>
2018-08-01 20:06:21 +0200

    link toggle css

--- script/app.js
+++ script/app.js
@@ -1,8 +1,8 @@
 
-$("#font-type").click(function() {
+$("#fntbtn").change(function() {
 	var $input = $( this );
-	console.log($input.prop( "checked" ));
-	if ($input.prop( "checked" ) == true) {
+	console.log($input.is(':checked'));
+	if ($input.is(':checked')) {
 		console.log("true");
 		$("p").toggleClass('gentleman cocktail');
 		$("h4").toggleClass('gentleman cocktail');
@@ -11,6 +11,6 @@ $("#font-type").click(function() {
 		$("p").toggleClass('cocktail gentleman');
 		$("h4").toggleClass('cocktail gentleman');
 	}
-});
+}).change();
 
 
