b1435f76ab92f21be71c3edddaebe7bff69ec6a4
unknown <stijn.van.der.spiegel@student.howest.be>
2018-08-01 21:10:23 +0200

    layout + jquery

--- script/app.js
+++ script/app.js
@@ -13,6 +13,25 @@ $(document).ready(function(){
 		$("h4").toggleClass('cocktail gentleman');
 	}
 	});
+	
+	$("#site-style").change(function() {
+	var $input = $( this );
+	console.log($input.is(':checked'));
+	if ($input.is(':checked')) {
+		console.log("true");
+		$("#controls").css('background-color', '#6fea6f');
+		$("#info").css('background-color', 'white');
+		$("#information").css('background-color', 'white');
+		$("#information").css('color', '#202020');
+		$("#imagedisplay").css('background-color', 'white');
+		$("input :text").css('color', '#6fea6f');
+		$("input :submit").css('color', '#6fea6f');
+	} else {
+		console.log("false");
+		$("p").toggleClass('cocktail gentleman');
+		$("h4").toggleClass('cocktail gentleman');
+	}
+	});
 
 });
 
