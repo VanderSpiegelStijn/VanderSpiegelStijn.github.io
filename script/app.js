49dc80cc6c47718937db4c727b4c15b9de525e95
unknown <stijn.van.der.spiegel@student.howest.be>
2018-08-01 19:47:37 +0200

    link jquery

--- script/app.js
+++ script/app.js
@@ -1,36 +1,16 @@
-var fontbtn;
-var sitebtn;
 
-$(document).ready(function(){
-   "use strict";
-
-	InitVars();
-	console.log(" init vars done");
-	console.log(fontbtn);
-	console.log(sitebtn);
-});
-
-
-
-function InitVars(){
-	fontbtn = $('#font-type')[0];
-	sitebtn = $('#site-style');
-}
+fontbtn.change(function() {
+	var $input = $( this );
+	console.log($input.prop( "checked" ));
+	if ($input.prop( "checked" ) == true) {
+		console.log("true");
+		$("p").toggleClass('gentleman cocktail');
+		$("h4").toggleClass('gentleman cocktail');
+	} else {
+		console.log("false");
+		$("p").toggleClass('cocktail gentleman');
+		$("h4").toggleClass('cocktail gentleman');
+	}
+}).change();
 
 
-if (fontbtn.is(':checked')) {
-	console.log("true");
-	$("p").toggleClass('gentleman cocktail');
-	$("h4").toggleClass('gentleman cocktail');
-} else {
-	console.log("false");
-	$("p").toggleClass('cocktail gentleman');
-	$("h4").toggleClass('cocktail gentleman');
-}
-
-if (sitebtn.is(':checked')) {
-	
-	
-} else {
-	
-}
